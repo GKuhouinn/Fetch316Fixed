f92c343aba353f13e85800e43145d8141f3b81a1
unknown <dong.wang01@yingxiong.com>
2025-02-17 16:55:40 +0800

    忘记导入log 包了
    忘记导入log 包了

--- fetch2/src/main/java/com/tonyodev/fetch2/downloader/SequentialFileDownloaderImpl.kt
+++ fetch2/src/main/java/com/tonyodev/fetch2/downloader/SequentialFileDownloaderImpl.kt
@@ -9,6 +9,7 @@ import com.tonyodev.fetch2core.*
 import java.io.*
 import java.net.HttpURLConnection
 import kotlin.math.ceil
+import android.util.Log
 
 class SequentialFileDownloaderImpl(private val initialDownload: Download,
                                    private val downloader: Downloader<*, *>,
