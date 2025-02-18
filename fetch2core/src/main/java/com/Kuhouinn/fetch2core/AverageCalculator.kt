bfa0d10aed89868f8ae7be7bdf75df643a1d51aa
unknown <dong.wang01@yingxiong.com>
2025-02-18 17:18:25 +0800

    修改一个用法解决编译错误
    修改一个用法解决编译错误

--- fetch2core/src/main/java/com/Kuhouinn/fetch2core/AverageCalculator.kt
+++ fetch2core/src/main/java/com/Kuhouinn/fetch2core/AverageCalculator.kt
@@ -87,7 +87,16 @@ class AverageCalculator @JvmOverloads constructor(
      * @return true if the passed in value was found in the values list. False otherwise.
      * */
     fun hasInputValue(value: Double): Boolean {
-        return valueList.contains(value)
+        //return valueList.contains(value)
+        if (count() < 1) {
+            return false
+        }
+        for (index in startIndex..endIndex) {
+            if (valueList[index] == value) {
+                return true
+            }
+        }
+        return false
     }
 
     /** Gets all values contained in the values list.
